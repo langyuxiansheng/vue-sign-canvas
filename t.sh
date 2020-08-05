8331235069ab3c203940205e9524a71c0b153229
zhouliujun <1096432931@qq.com>
2020-08-05 10:41:07 +0800

    更新sh

--- t.sh
+++ t.sh
@@ -1,3 +1,7 @@
+echo "设置 npm config set registry http://registry.npmjs.org"
+
+npm config set registry http://registry.npmjs.org
+
 echo "打包 => dist";
 
 # 打包 => dist
@@ -6,12 +10,16 @@ npm run build;
 echo "打包 插件lib";
 
 # 打包 插件
-# npm run lib;
+npm run lib;
 
-# echo "推送到npmjs";
+echo "推送到npmjs";
 
-# #推送到npmjs
-# npm publish;
+#推送到npmjs
+npm publish;
+
+echo "设置 npm config set registry https://registry.npm.taobao.org"
+
+npm config set registry https://registry.npm.taobao.org
 
 # 更新gh-pages
 echo "更新gh-pages";
